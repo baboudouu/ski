38cdd0d64980f18d67d7ee4502f8b6682e5c3358
Thibault Saunier <thibault.saunier@collabora.co.uk>
2011-01-11 14:33:25 +0100

    Clean up main, and create slope template

---
+++ slope.c
@@ -0,0 +1,1 @@
+#include "slope.h"
---
+++ slope.h
@@ -0,0 +1,14 @@
+#ifndef SLOPE_H_INCLUDED
+#define SLOPE_H_INCLUDED
+
+class Slope
+{
+    public:
+        Slope();
+        ~Slope();
+    protected:
+    private:
+
+};
+
+#endif // OVNI_H_INCLUDED
